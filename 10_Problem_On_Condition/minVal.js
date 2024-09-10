06f28219b5d467652b9b02f3f43454e7b0d9b2d2
jay1patel1 <jayp.fsd@outlook.com>
2024-09-10 09:36:36 +0530

    updated

---
+++ 10_Problem_On_Condition/minVal.js
@@ -0,0 +1,18 @@
+function minimum_Val(val1,val2,val3) {
+    
+    if (val1 < val2) {
+        if (val1 < val3) {
+            return val1;
+        } else {
+            return val3;
+        }
+    } else if (val2 < val3) {
+        return val2;
+    } else {
+        return val3;
+    }
+}
+
+console.log(minimum_Val(1,2,3));
+console.log(minimum_Val(2,1,3));
+console.log(minimum_Val(2,3,1));
