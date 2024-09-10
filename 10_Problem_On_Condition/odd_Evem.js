24bf01a095316c21085defab13ee2bf36e253687
jay1patel1 <jayp.fsd@outlook.com>
2024-09-10 09:28:02 +0530

    updated

---
+++ 10_Problem_On_Condition/odd_Evem.js
@@ -0,0 +1,12 @@
+function oddEven (num) {
+    if (num%2 === 0) {
+        console.log(true);
+        return true;
+    } else {
+        console.log(false);
+        return false;
+    }
+}
+
+oddEven (10);
+oddEven (5);
