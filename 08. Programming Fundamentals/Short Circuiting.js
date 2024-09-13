1ff9afa4ffa4647e90b9b7a8712f57e062b8cf07
jay1patel1 <jayp.fsd@outlook.com>
2024-09-13 07:23:03 +0530

    updated

---
+++ 08. Programming Fundamentals/Short Circuiting.js
@@ -0,0 +1,9 @@
+console.log(0 && 10);
+console.log(0 && 0);
+console.log(10 && 0);
+console.log(10 && 30);
+
+console.log(0 || 20);
+console.log(20 || 10);
+console.log(20 || 0);
+console.log(0 || 0);
