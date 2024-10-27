ec284a1d75f91774c837fb20e188af16fa8f0332
jay1patel1 <jayp.fsd@outlook.com>
2024-10-27 13:51:41 +0530

    updated

--- 08. Programming Fundamentals/DowloadFunWithPromise.js
+++ 08. Programming Fundamentals/DowloadFunWithPromise.js
@@ -1,39 +1,49 @@
-function downloadFiles (filename, filePath) {
+function downloadFiles (filename, filePath, cb, value) {
     console.log("Downloading " + filename + " file...");
     setTimeout(() => {
         console.log("Finished downloading " + filename + " file...");
-        return {
+        cb({
             filename: filename,
             filePath: filePath
-        }
+        }, value)
     }, 1000);
 }
 
-function updateFile (filename, filePath) {
-    console.log("Updating " + filename + " file...");
-    setTimeout(()=> {
-        console.log("Finished updating " + filename);
-    },200)
-}
+let abc = {
 
-function uploadFiles (filename, filePath) {
-    console.log("Uploading " + filename + " file...");
-    setTimeout(()=> {
-        console.log("Finished uploading " + filename);
-    },1200);
-}
+};
 
-const p1 = new Promise((resolve, reject) => {
-    console.log("start")
-    resolve ({
-        filename: "abc",
-        filePath: "path xyz",
-    })
-})
+downloadFiles("ABC","SDF", (obj,value) => {
+    value.filename = obj.filename;
+    value.filePath = obj.filePath;
+    console.log(abc);}, abc);
 
-let a = p1.then((value) => {
-    let temp = downloadFiles(value.filename, value.filePath);
-    return temp;
-})
 
-console.log(a)
+// function updateFile (filename, filePath) {
+//     console.log("Updating " + filename + " file...");
+//     setTimeout(()=> {
+//         console.log("Finished updating " + filename);
+//     },200)
+// }
+//
+// function uploadFiles (filename, filePath) {
+//     console.log("Uploading " + filename + " file...");
+//     setTimeout(()=> {
+//         console.log("Finished uploading " + filename);
+//     },1200);
+// }
+//
+// const p1 = new Promise((resolve, reject) => {
+//     console.log("start")
+//     resolve ({
+//         filename: "abc",
+//         filePath: "path xyz",
+//     })
+// })
+//
+// let a = p1.then((value) => {
+//     let temp = downloadFiles(value.filename, value.filePath);
+//     return temp;
+// })
+//
+// console.log(a)
