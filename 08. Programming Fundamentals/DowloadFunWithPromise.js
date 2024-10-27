90158252226e2d9bc604dba652ac86ad843426bd
jay1patel1 <jayp.fsd@outlook.com>
2024-10-27 15:09:13 +0530

    updated

--- 08. Programming Fundamentals/DowloadFunWithPromise.js
+++ 08. Programming Fundamentals/DowloadFunWithPromise.js
@@ -1,49 +1,39 @@
-function downloadFiles (filename, filePath, cb, value) {
-    console.log("Downloading " + filename + " file...");
-    setTimeout(() => {
-        console.log("Finished downloading " + filename + " file...");
-        cb({
-            filename: filename,
-            filePath: filePath
-        }, value)
-    }, 1000);
+function downloadFiles (filename, filePath) {
+    return new Promise((resolve, reject) => {
+        console.log("Downloading " + filename + " file...");
+        setTimeout(() => {
+            console.log("Finished downloading " + filename + " file.");
+            resolve ();
+        }, 1000);
+    })
 }
 
-let abc = {
+function updateFile (filename, filePath) {
+    return new Promise((resolve, reject) => {
+        console.log("Updating " + filename + " file...");
+        setTimeout(() => {
+            console.log("Finished updating " + filename + " file.");
+            resolve ();
+        }, 1000);
+    })
+}
 
-};
+function uploadFiles (filename, filePath) {
+    return new Promise((resolve, reject) => {
+        console.log("Uploading " + filename + " file...");
+        setTimeout(()=> {
+            console.log("Finished uploading " + filename);
+            resolve ();
+        },1200);
+    })
+}
 
-downloadFiles("ABC","SDF", (obj,value) => {
-    value.filename = obj.filename;
-    value.filePath = obj.filePath;
-    console.log(abc);}, abc);
+function seq (fileName, filePath) {
+    downloadFiles(fileName, filePath)
+        .then(() => {return updateFile(fileName, filePath)})
+        .then(() => {return uploadFiles(fileName, filePath)});
+}
 
-
-// function updateFile (filename, filePath) {
-//     console.log("Updating " + filename + " file...");
-//     setTimeout(()=> {
-//         console.log("Finished updating " + filename);
-//     },200)
-// }
-//
-// function uploadFiles (filename, filePath) {
-//     console.log("Uploading " + filename + " file...");
-//     setTimeout(()=> {
-//         console.log("Finished uploading " + filename);
-//     },1200);
-// }
-//
-// const p1 = new Promise((resolve, reject) => {
-//     console.log("start")
-//     resolve ({
-//         filename: "abc",
-//         filePath: "path xyz",
-//     })
-// })
-//
-// let a = p1.then((value) => {
-//     let temp = downloadFiles(value.filename, value.filePath);
-//     return temp;
-// })
-//
-// console.log(a)
+// seq("abc.text","xyz.com")
+let a = uploadFiles("xyz.text","abc.com")
+console.log(a);
