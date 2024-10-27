0f827ed668e68b4568ae545b19bf0a1aec90a970
jay1patel1 <jayp.fsd@outlook.com>
2024-10-27 23:12:47 +0530

    updated

--- 08. Programming Fundamentals/Promise3.js
+++ 08. Programming Fundamentals/Promise3.js
@@ -1,7 +1,23 @@
-let start = new Promise((resolve, reject) => {
-    resolve();
+let p1 = new Promise((resolve, reject) => {
+    console.log("P1 Started")
+    setTimeout(() => {
+        console.log("P1 Ended");
+        resolve(10);
+    },1000)
 })
 
-start.then(() => {
-
+p1.then((result) => {
+    console.log("First Started");
+    setTimeout(() => {
+        console.log("First Done");
+    },1000)
 })
+
+p1.then((result) => {
+    console.log("Second Started");
+    setTimeout(() => {
+        console.log("Second Done");
+    },500)
+})
+
+console.log("Main Code End")
