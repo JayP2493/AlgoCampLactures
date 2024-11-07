de5bd90a4ab0f62df3a59358dab891ab2a9ed7c6
jay1patel1 <jayp.fsd@outlook.com>
2024-11-07 22:23:52 +0530

    import export

--- 08. Programming Fundamentals/temp.js
+++ 08. Programming Fundamentals/temp.js
@@ -1,14 +1,48 @@
-function Events(dateOfEvents) {
-    this.dateOfEvents = dateOfEvents;
+function bubbleSort(arr) {
+    let len = arr.length;
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < len - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return arr;
 }
 
-Events.prototype.bookEvent = function () {
-    console.log("Booking Event");
+export function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
 }
 
-function Movie(movieName,movieDate) {
-    Events.call(this, movieDate);
-    this.movieName = movieName;
+export function selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            let temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+    return arr;
 }
 
-let abc = new Movie(10,15);
+export default bubbleSort;
---
+++ 08. Programming Fundamentals/temp2.js
@@ -0,0 +1,6 @@
+import def from './temp.js'
+import {selectionSort as xyz, insertionSort as abc} from './temp.js'
+
+console.log(def)
+console.log(abc)
+console.log(xyz)
